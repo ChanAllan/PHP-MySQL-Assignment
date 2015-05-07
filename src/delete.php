0e78a03283585c89ac34ff7305f6215962834a71
Allan Chan <chanal@onid.oregonstate.edu>
2015-05-07 00:57:47 -0700

    Contains the prepared statements to delete all rows in the table and to redirect back to index.php

---
+++ src/delete.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+include 'storedInfo.php';
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<!--Referred to http://webmaster.iu.edu/tools-and-guides/maintenance/redirect-meta-refresh.phtml
+			for redirecting pages-->
+		<meta http-equiv="refresh" content="0; URL=index.php" />
+		<title>PHP-Assignment-2</title>
+	</head>
+</html>
+
+<?php
+/*Code obtained from lecture to access my database and can be seen on: 
+http://php.net/manual/en/mysqli.quickstart.prepared-statements.php*/
+
+/* Referred to http://www.mustbebuilt.co.uk/php/insert-update-and-delete-with-mysqli/
+to set up DELETE statements*/
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "chanal-db", $myPassword, "chanal-db");
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} else {
+	if (!($stmt = $mysqli->prepare('DELETE FROM video_store'))) {
+		echo 'Prepared failed<br><br>';
+	}
+	if(!($stmt->execute())) {
+		echo 'Execute failed<br><br>';
+	}
+}
+
+$stmt->close();
+///////////*WILL REMOVE MYSQL ERROR  OUTPUTS BEFORE SUBMISSION*/////////////////////////////////////
+
+
+
+?>
