11d1549d550e1e9461e5ee004aa700b60538dd89
Allan Chan <chanal@onid.oregonstate.edu>
2015-05-07 00:56:43 -0700

    Contains the CREATE TABLE query for the database

---
+++ src/video_store.sql
@@ -0,0 +1,9 @@
+CREATE TABLE video_store (
+id int(11) NOT NULL AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+category varchar(255),
+length int(11),
+rented BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id),
+UNIQUE KEY (name)
+) ENGINE=InnoDB;
